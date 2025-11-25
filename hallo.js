c66d0875f694d24f92a65c8a31c1614306afb0a4
fitriyanto001 <fitriyanto25112003@gmail.com>
2025-11-25 16:18:55 +0800

    Add files via upload

---
+++ hallo.js
@@ -0,0 +1,13 @@
+ var nama = "anto";
+    var umur = 78;
+    var aktif = true;
+
+    var x = 10;
+    var y = 5;
+    var hasil = x + y;
+
+    if (umur>18){
+        alert("Hai" + nama+" menjadi orang yang bisa kultivasi");
+    } else {
+        alert("Hai" +nama+" ingin menjadi orang biasa");
+    }
---
+++ sapa.js
@@ -0,0 +1,4 @@
+var nama = document.getElementById("inputNama").value;
+function sapa() {
+    alert("Halo, "+nama+" selamat datang di praktikum JavaScript!");
+}
